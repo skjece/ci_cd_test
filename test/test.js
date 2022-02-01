fae4d71c6977409078654c8c56d6596e1d696f1e
Saurabh Jain <skjece@gmail.com>
2022-02-02 03:42:50 +0530

    script clarified

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("responds with Hello World!", (done) => {
-    request(app).get("/").expect("Hello World!", done);
+  it("responds with Hello Saurabh!", (done) => {
+    request(app).get("/").expect("Hello Saurabh!", done);
   });
 });
