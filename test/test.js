2fee6991a4b85574baeb0df862ebcce5d0253117
Saurabh Jain <skjece@gmail.com>
2022-02-02 14:11:52 +0530

    git pull

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("responds with Hello Bossssssss!", (done) => {
-    request(app).get("/").expect("Hello Bossssssss!", done);
+  it("responds with Hello !", (done) => {
+    request(app).get("/").expect("Hello !", done);
   });
 });
